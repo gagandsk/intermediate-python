3b004e25bf79d4bac90e19598fa9425ce8f4f195
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-05-02 10:52:27 +0200

    List Comprehension

---
+++ 04-list.py
@@ -0,0 +1,8 @@
+numbers = [element for element in range(1,11)]
+print(numbers)
+
+numbers_V2 = [element * 2 for element in range(1,11)]
+print(numbers_V2)
+
+numbers_V3 = [i * 2 for i in range(1,101) if i % 2 == 0]
+print(numbers_V3)
