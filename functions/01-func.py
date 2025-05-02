b104f57f8e4aea278fbe9d56dd04a6feed6c2906
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-05-02 12:31:59 +0200

    functions

---
+++ functions/01-func.py
@@ -0,0 +1,17 @@
+print("Hola Mundo")
+
+def my_print(text):
+    print(text)
+    
+my_print('este es my texto')
+my_print('hola')
+
+a = 10
+b = 90
+
+def suma(a, b):
+    my_print(a + b)
+
+suma(a, b)
+suma(1, 5)
+suma(10, 10)
