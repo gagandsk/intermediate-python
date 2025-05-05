39475e77a2753a6ab96fb487b64425255a87cbe3
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-05-05 12:29:47 +0200

    add basic error and exception handling

---
+++ file-manipulation/02-errors.py
@@ -0,0 +1,14 @@
+# print(0 / 0))
+# print(result)
+print('Hola')
+
+suma = lambda x,y: x + y
+# suma = lambda x,y: x + (y * 2)
+assert suma(2,2) == 4
+print('Hola 2')
+
+age = 10
+if age < 18:
+    raise Exception('Eres menor de edad')
+
+print('Hola 3')
