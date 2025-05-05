44aa1baefa6ccb8b308c05530d8f097d55b3a9c2
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-05-05 11:08:49 +0200

    handle module duality (script vs importable)

--- app/main.py
+++ app/main.py
@@ -1,28 +1,33 @@
 import utils
 
-keys, values = utils.get_population()
-print(keys, values)
-print(utils.A)
-
 data = [
-    {
-        'Country': 'Colombia',
-        'Population': 300
-    },
-    {
-        'Country': 'Bolivia',
-        'Population': 400
-    },
-    {
-        'Country': 'Ecuador',
-        'Population': 500
-    },
-    {
-        'Country': 'España',
-        'Population': 600
-    }
-]
+        {
+            'Country': 'Colombia',
+            'Population': 300
+        },
+        {
+            'Country': 'Bolivia',
+            'Population': 400
+        },
+        {
+            'Country': 'Ecuador',
+            'Population': 500
+        },
+        {
+            'Country': 'España',
+            'Population': 600
+        }
+    ]
 
-country = input('Que país desea buscar? ')
-result = utils.population_by_country(data, country)
-print(result)
+def run():
+    keys, values = utils.get_population()
+    print(keys, values)
+
+
+    country = input('Que país desea buscar? ')
+    result = utils.population_by_country(data, country)
+    print(result)
+    
+#si es ejecutado desde la terminal, que ejecute el metodo 'run'
+if __name__ == '__main__':
+    run()
--- app/utils.py
+++ app/utils.py
@@ -3,8 +3,6 @@ def get_population():
     values = [300, 400]
     return keys, values
 
-A = 'Hola'
-
 def population_by_country(data, country):
     result = list(filter(lambda item: item['Country'] == country, data))
     return result
