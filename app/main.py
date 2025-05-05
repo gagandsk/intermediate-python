2360489a30efada498b9618b85698957b9c3cdb5
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-05-05 16:43:17 +0200

    create pie charts with filtered data

--- app/main.py
+++ app/main.py
@@ -5,14 +5,13 @@ import charts
 def run():
     
   data = read_csv.read_csv('data_set2.csv')
-  '''
-  data = list(filter(lambda item : item['Continent'] == 'South America',data))
+  data = list(filter(lambda item : item['Continent'] == 'Europe',data))
 
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_chart(countries, percentages)
-    '''
-  
+    
+  '''
   country = input('Type Country => ')
 
   result = utils.population_by_country(data, country)
@@ -21,7 +20,7 @@ def run():
     country = result[0]
     labels, values = utils.get_population(country)
     charts.generate_pie_chart(labels, values)
-  
+  '''
     
 #si es ejecutado desde la terminal, que ejecute el metodo 'run'
 if __name__ == '__main__':
